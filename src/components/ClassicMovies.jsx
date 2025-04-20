65d9fff65bbfc1961f1378e78da369f71d10259d
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-04-20 08:23:18 +0600

    added the section called 'Classic Movies' in the home page

--- src/components/ClassicMovies.jsx
+++ src/components/ClassicMovies.jsx
@@ -2,10 +2,132 @@ import React from "react";
 
 const ClassicMovies = () => {
   return (
-    <div>
+    <div >
       <h1 className="text-yellow-300 font-bold font-winky text-2xl pt-2">
         Classic Movies
       </h1>
+      <div className="text-gray-300 lg:w-3/4 mx-auto mt-10 mb-20">
+      <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+  <li>
+    <div className="timeline-middle ">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </div>
+    <div className="timeline-start mb-10 md:text-end">
+      <time className="font-mono italic">1939</time>
+      <div className="text-lg font-black">Gone with the Wind</div>
+      Directed by Victor Fleming, Gone with the Wind is a grand historical romance set during the American Civil War and Reconstruction era. It follows the turbulent life of Scarlett O’Hara, a strong-willed Southern belle, as she faces love, loss, and survival in a rapidly changing world. With stunning cinematography, lavish costumes, and powerful performances by Vivien Leigh and Clark Gable, the film became a cultural phenomenon. It won 10 Academy Awards, including Best Picture, and remains one of the highest-grossing films of all time when adjusted for inflation.
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </div>
+    <div className="timeline-end md:mb-10">
+      <time className="font-mono italic">1941</time>
+      <div className="text-lg font-black">Citizen Kane</div>
+      Often hailed as the greatest film ever made, Citizen Kane was the groundbreaking directorial debut of Orson Welles. The film tells the story of Charles Foster Kane, a powerful newspaper tycoon whose final word—“Rosebud”—prompts a journalist to investigate his life. Through a series of interviews and flashbacks, the film paints a complex picture of a man torn between ambition and emptiness. With its innovative use of deep focus, lighting, and narrative structure, Citizen Kane set a new standard for cinematic storytelling and remains a touchstone in film studies.
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </div>
+    <div className="timeline-start mb-10 md:text-end">
+      <time className="font-mono italic">1942</time>
+      <div className="text-lg font-black">Casablanca </div>
+      Set in Nazi-occupied Morocco during World War II, Casablanca tells the story of Rick Blaine, an American nightclub owner who must choose between rekindling his romance with former lover Ilsa and helping her husband escape to continue his fight against the Nazis. Directed by Michael Curtiz and starring Humphrey Bogart and Ingrid Bergman, the film blends romance, intrigue, and political tension. It won three Academy Awards, including Best Picture, and has become a staple of classic cinema thanks to its memorable lines, unforgettable music, and timeless themes of love and sacrifice.
+
+
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </div>
+    <div className="timeline-end md:mb-10">
+      
+      <time className="font-mono italic">1957</time>
+      
+      <div className="text-lg font-black">12 Angry Men</div>
+      Directed by Sidney Lumet, 12 Angry Men is a gripping courtroom drama that unfolds entirely in a jury room, where twelve men deliberate the fate of a teenage boy accused of murder. Initially convinced of his guilt, the jurors are forced to confront their own prejudices and assumptions as one man begins to question the evidence. With a minimalist setting and a powerhouse performance by Henry Fonda, the film is a masterclass in tension, character development, and moral reasoning. It remains a profound exploration of justice, doubt, and human behavior.
+
+   </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </div>
+    <div className="timeline-start mb-10 md:text-end">
+    <time className="font-mono italic">1972</time>
+    <div className="text-lg font-black">The Godfather</div>
+    Francis Ford Coppola’s The Godfather is a powerful tale of family, crime, and loyalty, chronicling the rise of Michael Corleone within his father’s mafia empire. With iconic performances by Marlon Brando, Al Pacino, and James Caan, the film explores themes of power, corruption, and the American Dream. Its haunting score, masterful direction, and compelling characters helped redefine the gangster genre. Winner of three Academy Awards, including Best Picture, The Godfather is considered one of the most influential and revered films in cinema history.
+    </div>
+  </li>
+</ul>
+      </div>
     </div>
   );
 };
